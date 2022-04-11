394b0da28d8d638dbfb088ddcc73dccd9da3c89a
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-04-11 14:39:17 +0600

    update

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data =await _context.Actors.ToListAsync();
+            return View(data);
+        }
+    }
+}
