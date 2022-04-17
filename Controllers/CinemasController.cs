9568bfe8e4cc864f55849c5f568d3bb53442ce45
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-04-17 11:52:18 +0600

    adding cinemas view

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -18,7 +18,7 @@ namespace eTicket.Controllers
         public async Task<IActionResult> Index()
         {
             var data = await _context.Cinemas.ToListAsync();
-            return View();
+            return View(data);
         }
     }
 }
