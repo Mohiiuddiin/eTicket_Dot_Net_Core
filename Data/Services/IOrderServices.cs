309bc4de2508e681f0b3c8dbe34141cc50b6396b
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-06-25 16:25:07 +0600

    update order_service

--- Data/Services/IOrderServices.cs
+++ Data/Services/IOrderServices.cs
@@ -10,7 +10,6 @@ namespace eTicket.Data.Services
     {
         Task StoreOrderAsync(List<ShoppingCartItem> items,string userId,string userEmailAddress);
         Task<List<Order>> GetOrdersByUserIdAsync(string userId);
-        //
-
+        
     }
 }
