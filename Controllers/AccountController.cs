0eb21cc504b8a778a41ebd33eefef6b9167637d8
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-07-04 15:23:36 +0600

    login_feature_added

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -24,6 +24,32 @@ namespace eTicket.Controllers
             _context = context;
         }
         public IActionResult Login() => View(new LoginVM());
-        
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user!=null)
+            {
+                var isMatchPass = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (isMatchPass)
+                {
+                    var res = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (res.Succeeded)
+                    {
+                        return RedirectToAction("Index","Movies");
+                    }
+                }
+                else
+                {
+                    TempData["Error"] = "wrong credentials, try again!";
+                    return View(loginVM);
+                }
+            }
+
+            TempData["Error"] = "wrong credentials, try again!";
+            return View(loginVM);
+        }
     }
 }
