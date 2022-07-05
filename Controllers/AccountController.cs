c687477392db7b43db69296677bac62a43465154
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-07-05 11:16:26 +0600

    sign_up_form

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eTicket.Data;
+using eTicket.Data.Static;
 using eTicket.Data.ViewModels;
 using eTicket.Models;
 using Microsoft.AspNetCore.Identity;
@@ -53,5 +54,34 @@ namespace eTicket.Controllers
         }
 
         public IActionResult SignUp() => View(new SignUpVM());
+        [HttpPost]
+        public async Task<ActionResult> SignUp(SignUpVM signUpVM)
+        {
+            if (!ModelState.IsValid)
+            {                
+                return View(signUpVM);
+            }
+            var user = await _userManager.FindByEmailAsync(signUpVM.EmailAddress);
+            if (user!=null)
+            {
+                TempData["Error"] = "This email address is already exist!";
+                return View(signUpVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = signUpVM.FullName,
+                Email = signUpVM.EmailAddress,
+                UserName = signUpVM.EmailAddress
+            };
+            var response = await _userManager.CreateAsync(newUser,signUpVM.Password);
+            if (response.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            
+            return View("Registered Comppleted");
+        }
+        
     }
 }
